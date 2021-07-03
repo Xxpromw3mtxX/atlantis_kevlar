78d8429e93a0cdc525e17dc923bdcd111f8e3879
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-03 11:27:36 +0200

    changes three lines into already equipped

--- lfKevlar/locales/it.lua
+++ lfKevlar/locales/it.lua
@@ -1,7 +1,5 @@
 Locales ['it'] = {
-    ['light_equipped'] = 'Hai già un\'armatura leggera equipaggiata!',
-    ['medium_equipped'] = 'Hai già un\'armatura media equipaggiata!',
-    ['heavy_equipped'] = 'Hai già un\'armatura pesante equipaggiata!',
+    ['already_equipped'] = 'Hai già un\'armatura equipaggiata!',
     ['light_equip'] = 'Armatura leggera equipaggiata!',
     ['medium_equip'] = 'Armatura media equipaggiata!',
     ['heavy_equip'] = 'Armatura pesante equipaggiata!',
