7ee4345b9ce138840b6da291a5c94f0ba1d693f0
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-03 11:28:29 +0200

    solved a problem where armor where wasted

--- lfKevlar/client/cl_script.lua
+++ lfKevlar/client/cl_script.lua
@@ -11,14 +11,14 @@ Citizen.CreateThread(function()
 	end
 end)
 
-function equipArmor(type, weight)
+function equipArmor(item, weight)
     SetPedArmour(GetPlayerPed(-1), 0)
     ClearPedBloodDamage(GetPlayerPed(-1))
     ResetPedVisibleDamage(GetPlayerPed(-1))
     ClearPedLastWeaponDamage(GetPlayerPed(-1))
     ResetPedMovementClipset(GetPlayerPed(-1), 0)
 
-    TriggerServerEvent('lfKevlar:armourRemoveItem', type)
+    TriggerServerEvent('lfKevlar:armourRemoveItem', item)
     TriggerEvent('skinchanger:getSkin', function(skin)
         if skin.sex == 0 then
             TriggerEvent('skinchanger:loadClothes', skin, {['bproof_1'] = 1,  ['bproof_2'] = 1})
@@ -32,8 +32,8 @@ end
 
 RegisterNetEvent('lfKevlar:equipLightArmour')
 AddEventHandler('lfKevlar:equipLightArmour', function()
-    if PedArmour == 20 then
-        TriggerEvent('mythic_notify:client:SendAlert', {type = 'error', text = _U('light_equipped'), length = 2500})
+    if PedArmour >= 20 then
+        TriggerEvent('mythic_notify:client:SendAlert', {type = 'error', text = _U('already_equipped'), length = 2500})
     else
         equipArmor(light, 20)
         TriggerEvent('mythic_notify:client:SendAlert', {type = 'success', text = _U('light_equip'), length = 2500})
@@ -42,20 +42,20 @@ end)
 
 RegisterNetEvent('lfKevlar:equipMediumArmour')
 AddEventHandler('lfKevlar:equipMediumArmour', function()
-    if PedArmour == 40 then
-        TriggerEvent('mythic_notify:client:SendAlert', {type = 'error', text = _U('medium_equipped'), length = 2500})
+    if PedArmour >= 40 then
+        TriggerEvent('mythic_notify:client:SendAlert', {type = 'error', text = _U('already_equipped'), length = 2500})
     else
-        equipArmor(light, 40)
+        equipArmor(medium, 40)
         TriggerEvent('mythic_notify:client:SendAlert', {type = 'success', text = _('medium_equip'), length = 2500})
     end
 end)
 
 RegisterNetEvent('lfKevlar:equipHeavyArmour')
 AddEventHandler('lfKevlar:equipHeavyArmour', function()
-    if PedArmour == 100 then
-        TriggerEvent('mythic_notify:client:SendAlert', {type = 'error', text = _U('heavy_equipped'), length = 2500})
+    if PedArmour >= 100 then
+        TriggerEvent('mythic_notify:client:SendAlert', {type = 'error', text = _U('already_equipped'), length = 2500})
     else
-        equipArmor(light, 100)
+        equipArmor(heavy, 100)
         TriggerEvent('mythic_notify:client:SendAlert', {type = 'success', text = _U('heavy_equip'), length = 2500})
     end
 end)
