2d16262d38a16ea7705d1028eda510f9c8c1cc64
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-04 19:00:55 +0200

    added more languages

---
+++ lfKevlar/locales/fr.lua
@@ -0,0 +1,6 @@
+Locales ['fr'] = {
+    ['already_equipped'] = 'Vous avez déjà une armure équipée !',
+    ['light_equip'] = 'Armure légère équipée !',
+    ['medium_equip'] = 'Armure moyenne équipée !',
+    ['heavy_equip'] = 'Armure lourde équipée !',
+}
