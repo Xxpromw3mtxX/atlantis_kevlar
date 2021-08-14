38a1c367e1af64bf983cda9daa912381d8b71eaa
Francesco Metta <fra.metta@hotmail.it>
2021-08-14 21:37:24 +0200

    new sql file

--- sql/items.sql
+++ sql/items.sql
@@ -1,4 +1,5 @@
-INSERT INTO `items` (`name`, `label`, `weight`) VALUES 
-(`Light`, `Light armour`, 2000),
-(`Medium`, `Medium armour`, 5500),
-(`Heavy`, `Heavy armour`, 8500);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES (
+    'light', 'Armatura leggera', '1', '0', '1'), 
+    ('medium', 'Armatura media', '1', '0', '1'), 
+    ('heavy', 'Armatura pesante', '1', '0', '1')
+;
