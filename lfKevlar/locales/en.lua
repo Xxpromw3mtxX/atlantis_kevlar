0510a93947f167ad9287040b99237d11a566bbdf
Francesco Metta <fra.metta@hotmail.it>
2021-08-14 21:37:17 +0200

    old lfKevlar

--- lfKevlar/locales/en.lua
+++
@@ -1,6 +0,0 @@
-Locales ['en'] = {
-    ['already_equipped'] = 'You already have an armor equipped!',
-    ['light_equip'] = 'Light armor equipped!',
-    ['medium_equip'] = 'Medium armor equipped!',
-    ['heavy_equip'] = 'Heavy armor equipped!',
-}
--- lfKevlar/locales/it.lua
+++
@@ -1,6 +0,0 @@
-Locales ['it'] = {
-    ['already_equipped'] = 'Hai già un\'armatura equipaggiata!',
-    ['light_equip'] = 'Armatura leggera equipaggiata!',
-    ['medium_equip'] = 'Armatura media equipaggiata!',
-    ['heavy_equip'] = 'Armatura pesante equipaggiata!',
-}
