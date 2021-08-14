80845017879818f59fa0ece12d6e8fa399b3d7f9
Francesco Metta <fra.metta@hotmail.it>
2021-08-14 21:37:33 +0200

    new server script file

---
+++ atlantis_kevlar/server/sv_script.lua
@@ -0,0 +1,38 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj)
+	ESX = obj
+end)
+
+RegisterServerEvent('atlantis_kevlar:armourRemoveItem')
+AddEventHandler('atlantis_kevlar:armourRemoveItem', function(item)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.removeInventoryItem(item, 1)
+end)
+
+ESX.RegisterUsableItem(Config['light'], function(source)
+	TriggerClientEvent('atlantis_kevlar:equipLightArmour', source)
+end)
+
+ESX.RegisterUsableItem(Config['medium'], function(source)
+	TriggerClientEvent('atlantis_kevlar:equipMediumArmour', source)
+end)
+
+ESX.RegisterUsableItem(Config['heavy'], function(source)
+	TriggerClientEvent('atlantis_kevlar:equipHeavyArmour', source)
+end)
+
+RegisterServerEvent('atlantis_kevlar:armourRemove')
+AddEventHandler('atlantis_kevlar:armourRemove', function(armourWeight)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    TriggerClientEvent('atlantis_kevlar:armourRemoveClient', source)
+
+	if armourWeight <= Config['light_weight'] then
+		xPlayer.addInventoryItem(Config['light'], 1)
+	elseif armourWeight > Config['light_weight'] and armourWeight <= Config['medium_weight'] or armourWeight < Config['heavy_weight'] then
+		xPlayer.addInventoryItem(Config['medium'], 1)
+	elseif armourWeight == Config['heavy_weight'] then
+		xPlayer.addInventoryItem(Config['heavy'], 1)
+	end
+end)
