5cb3f7efee4d51b57e2cdb861182b7e91713abff
Francesco Metta <fra.metta@hotmail.it>
2021-08-16 00:28:44 +0200

    merged from XenoS
    After testing, I've decided to implement the XenoS (https://github.com/XenoS-ITA/atlantis_kevlar/blob/main/atlantis_kevlar/server/sv_script.lua) method of registering items. Still using the old method for krz_personalmenu.

--- atlantis_kevlar/server/sv_script.lua
+++ atlantis_kevlar/server/sv_script.lua
@@ -10,29 +10,25 @@ AddEventHandler('atlantis_kevlar:armourRemoveItem', function(item)
     xPlayer.removeInventoryItem(item, 1)
 end)
 
-ESX.RegisterUsableItem(Config['light'], function(source)
-	TriggerClientEvent('atlantis_kevlar:equipArmour', source, Config['light'], Config['light_weight'])
-end)
+for k, v in pairs(Config.Kevlar) do
+	ESX.RegisterUsableItem(k, function(source)
+		TriggerClientEvent('atlantis_kevlar:equipArmour', source, k, v)
+	end)
+end
 
-ESX.RegisterUsableItem(Config['medium'], function(source)
-	TriggerClientEvent('atlantis_kevlar:equipArmour', source, Config['medium'], Config['medium_weight'])
-end)
 
-ESX.RegisterUsableItem(Config['heavy'], function(source)
-	TriggerClientEvent('atlantis_kevlar:equipArmour', source, Config['heavy'], Config['heavy_weight'])
-end)
-
+-- krz_personalmenu settings DON'T MESS WITH THEM
 RegisterServerEvent('atlantis_kevlar:armourRemove')
 AddEventHandler('atlantis_kevlar:armourRemove', function(armourWeight)
     local xPlayer = ESX.GetPlayerFromId(source)
 
     TriggerClientEvent('atlantis_kevlar:armourRemoveClient', source)
-
-	if armourWeight <= Config['light_weight'] then
+	
+	if armourWeight <= Config.Kevlar['light'] then
 		xPlayer.addInventoryItem(Config['light'], 1)
-	elseif armourWeight > Config['light_weight'] and armourWeight <= Config['medium_weight'] or armourWeight < Config['heavy_weight'] then
+	elseif armourWeight > Config.Kevlar['light'] and armourWeight <= Config.Kevlar['medium'] or armourWeight < Config.Kevlar['heavy'] then
 		xPlayer.addInventoryItem(Config['medium'], 1)
-	elseif armourWeight == Config['heavy_weight'] then
+	elseif armourWeight == Config.Kevlar['heavy'] then
 		xPlayer.addInventoryItem(Config['heavy'], 1)
 	end
 end)
