1c8a09fdd396883e35d1f8d32b02decabb56c0cf
Francesco Metta <fra.metta@hotmail.it>
2021-08-16 00:26:39 +0200

    modified how the config works

--- atlantis_kevlar/config.lua
+++ atlantis_kevlar/config.lua
@@ -4,14 +4,16 @@ Config = {}
 Config.Locale = 'it'
 
 -- Using mythic_notify?
-Config.mythic = true
+Config.mythic = true -- If you're not using mythic, it will use the default ESX notification system
+
+-- Customize the amount of shield that each Kevlar vest guarantee
+Config.Kevlar = {
+    ['light'] = 20,
+    ['medium'] = 40,
+    ['heavy'] = 100
+}
 
 -- Kevlar names, needed if using krz_personalmenu
 Config['light'] = 'light'  -- Change if you want to change the name of the light armour
 Config['medium'] = 'medium' -- Change if you want to change the name of the medium armour
 Config['heavy'] = 'heavy' -- Change if you want to change the name of the heavy armour
-
--- Customize the amount of shield that each Kevlar vest guarantee
-Config['light_weight'] = 20 -- Change the weight of the light armour
-Config['medium_weight'] = 40 -- Change the weight of the medium armour
-Config['heavy_weight'] = 100 -- Change the weight of the heavy armour
