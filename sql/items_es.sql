1bd487493156f85d38bec33f5842d634ea505e9f
Antho7 <62178203+Antho7Twitch@users.noreply.github.com>
2021-08-15 18:18:35 +0200

    Create items_es.sql

---
+++ sql/items_es.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES 
+    ('light', 'Chaleco ligero', '1', '0', '1'), 
+    ('medium', 'Chaleco medio', '1', '0', '1'), 
+    ('heavy', 'Chaleco pesado', '1', '0', '1')
+;
